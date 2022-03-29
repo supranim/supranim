9c0658c8ee8d9f2c318617fa8f00a773d03e4460
George Lemon <georgelemon@protonmail.com>
2022-03-29 16:29:46 +0300

    Supranim app
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim.nim
+++ src/supranim.nim
@@ -7,61 +7,46 @@
 #          Website https://supranim.com
 #          Github Repository: https://github.com/supranim
 
-from os import getAppDir, normalizedPath
+from os import getAppDir, normalizedPath, getCurrentDir, fileExists
 
+import jsony
 import std/[asyncdispatch, options, times]
 import supranim/[application, router, server]
-import jsony
-
-# import supranim/cache/memcache
-# import supranim/events/subscriber
-
-## Import Built-in Service Providers
-from supranim/session/cookiejar import CookieServiceProvider
 import supranim/services
+import supranim/http/assets
 
 export Port
-export Application, application
+export App, application
 export HttpCode, HttpMethod, Request, Response
-export send, send404, send500, json, json404, json500, redirect, redirect302
+export response, send404, send500, json, json404, json500, redirect, redirect302, view
+export server.getParams
+
 export jsony
 
-# Database Environment Configuration
-# os.putEnv("DB_HOST", "127.0.0.1")
-# os.putEnv("DB_NAME", "vasco")
-# os.putEnv("DB_USER", "postgres")
-# os.putEnv("DB_PASS", "postgres")
-
-proc onRequest(req: Request, res: Response): Future[ void ] =
+proc onRequest(req: var Request, res: Response): Future[ void ] =
     # Determine the type of the requested method, search for it and
     # return the response if found, otherwise, return a 404 response.
     {.gcsafe.}:
         let path = req.path.get()
         var reqRoute = path
         if req.httpMethod == some(HttpGet):
-            var fixTrailingSlash = false
-            if reqRoute[^1] == '/':
-                reqRoute = path[0 .. ^2]
-                fixTrailingSlash = true
-            if Router.getExists(reqRoute):
-                if fixTrailingSlash:
-                    res.redirect(reqRoute)
-                else:
-                    Router.getRoute(reqRoute).runCallable(req, res)
+            let metaRouteTuple: tuple[status: bool, key: string, isDynamic: bool] = Router.existsRuntime(HttpGet, reqRoute)
+            if metaRouteTuple.status == true:
+                let isDynamicRoute = metaRouteTuple.isDynamic
+                var routeInstance: Route = Router.getRoute(metaRouteTuple.key, isDynamicRoute)
+                if isDynamicRoute:
+                    req.setParams(routeInstance.getRouteParams())
+                routeInstance.runCallable(req, res)
                 return
             res.send404()
             return
 
-        if req.httpMethod == some(HttpPost):
-            if Router.getExists(reqRoute):
-                res.json("hello")
-                return
-            res.json404()
-            return
+        # if req.httpMethod == some(HttpPost):
+        #     if Router.getExists(reqRoute):
+        #         res.json("hello")
+        #         return
+        #     res.json404()
+        #     return
 
-proc start*[A: Application](newApp: var A) =
-    ## Procedure for starting your Supranim Application
-    ## with current configuration settings and available service providers.
-    # Initialize Services via Service Provider
-    # cookiejar.enable(enableHashKeys=true, enableEncryption=true)
-    run(onRequest, newApp)
+proc start*[A: Application](app: var A) =
+    run(onRequest, app)
--- src/supranim/application.nim
+++ src/supranim/application.nim
@@ -1,28 +1,53 @@
+import nyml
+
 from std/nativesockets import Domain
 from std/net import Port
 from std/logging import Logger
+from std/os import getCurrentDir, putEnv, getEnv, fileExists
+from std/strutils import toUpperAscii
+
 export Port
 
+include supranim/db
+
 type
+    AssetsTuple = ref tuple[source, public: string]
+
     Application* = object
         port: Port                  ## Specify a port or let retrieve one automatically
         address: string             ## Specify a local IP address
         domain: Domain
         ssl: bool                   ## Boot in SSL mode (requires HTTPS Certificate)
         threads: int                ## Boot Supranim on a specific number of threads
+        database: DBConfig
+        assets: AssetsTuple
+        views: string               ## Path to Views source directory
         recyclable: bool            ## Whether to reuse current port or not
         loggers: seq[Logger]
 
-proc init*[A: typedesc[Application]](app: A, port = Port(3399), address = "localhost", ssl = false, threads = 1): Application =
-    ## Inititalize Application
-    result = app(
-        address: address,
-        domain: Domain.AF_INET,
-        port: port,
-        ssl: ssl,
-        threads: threads,
-        recyclable: true
-    )
+const yamlEnvFile = ".env.yml"
+
+var App* {.threadvar.}: Application
+App = Application()
+
+proc parseEnvFile(envPath: string): Document =
+    ## Parse and validate .env.yml configuration file
+    var yml = Nyml.init(contents = readFile(envPath))
+    result = yml.toJson()
+
+proc init*(port = Port(3399), address = "localhost", ssl = false, threads = 1): Application =
+    ## Initialize Supranim Application
+    let doc: Document = parseEnvFile(envPath = getCurrentDir() & "/bin/" & yamlEnvFile)
+    ## Set DB Environment credentials
+    for dbEnv in @["host", "prefix", "name", "user", "password"]:
+        putEnv("DB_" & toUpperAscii(dbEnv), doc.get("database.main." & dbEnv).getStr)
+    # testDb()
+    App.address = address
+    App.domain = Domain.AF_INET
+    App.port = Port(doc.get("app.port").getInt)
+    App.threads = doc.get("app.threads").getInt
+    App.recyclable = true
+    result = App
 
 proc getAddress*[A: Application](app: A): string {.inline.}  =
     ## Get the local address
@@ -36,19 +61,62 @@ proc hasSSL*[A: Application](app: A): bool {.inline.}  =
     ## Determine if ssl is turned on
     result = app.ssl
 
+proc hasDatabase*[A: Application](app: A): bool {.inline.} =
+    ## Determine if application has a database attached
+    result = app.database != nil    
+
+proc hasMultiDatabase*[A: Application](app: A): bool {.inline.} =
+    ## Determine if application has multi databases attached
+    result = app.secondary.len != 0
+
 proc isMultithreading*[A: Application](app: A): bool {.inline.}  =
     ## Determine if application runs on multiple threads
-    result = app.threads != 0 and app.threads != 1
+    result = app.threads notin {0, 1}
 
 proc getThreads*[A: Application](app: A): int {.inline.} =
+    ## Get number of available threads
     result = app.threads
 
 proc getLoggers*[A: Application](app: A): seq[Logger] =
+    ## Retrieve all logger instances
     result = app.loggers
 
 proc getDomain*[A: Application](app: A): Domain =
+    ## Retrieve Supranim domain instance
     result = app.domain
+
+proc getViewContent*(app: var Application, key: string, layout = "base"): string =
+    ## Retrieve contents of a specific view by view id.
+    let pathView = getCurrentDir() & "/assets/" & key & ".html"
+    if fileExists(pathView):
+        result = readFile(pathView)
 
 proc isRecyclable*[A: Application](app: A): bool =
     ## Determine if current application Port can be reused
     result = app.recyclable
+
+proc printBootStatus*[A: Application](app: A) =
+    ## Print boot status of the current application instance
+    echo "----------------- ⚡️ -----------------"
+    echo("👌 Up & Running on http://", app.getAddress&":"& $app.getPort)
+    
+    var defaultCompileOptions: seq[string]
+    when compileOption("opt", "size"):
+        defaultCompileOptions.add("Compiled with Size Optimization")
+    when defined(release):
+        defaultCompileOptions.add("Release mode: Production")
+    else:
+        defaultCompileOptions.add("Release mode: Development")
+    when compileOption("threads"):
+        defaultCompileOptions.add("Multi-threading: true (threads: " & $app.getThreads & ")")
+    else:
+        defaultCompileOptions.add("Multi-threading: false")
+
+    for compileOptionLabel in defaultCompileOptions:
+        echo "✓ " & compileOptionLabel
+
+    # echo "✓ Static Assets Proxy: Yes"
+    # echo "✓ Release mode: Development"
+    # echo "--------- Service Providers ----------"
+    # for loadedService in loadedServices:
+    #     echo("✓ ", loadedService)
