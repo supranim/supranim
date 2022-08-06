678b9fdd6b75e5572de9c8bd6a16a27fc474e266
George Lemon <georgelemon@protonmail.com>
2022-08-06 15:57:07 +0300

    cleanup
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim/supplier.nim
+++
@@ -1,8 +0,0 @@
-# Supranim is a simple MVC-style web framework for building
-# fast web applications, REST API microservices and other cool things.
-#
-# (c) 2021 Supranim is released under MIT License
-#          George Lemon | Made by Humans from OpenPeep
-#          https://supranim.com   |    https://github.com/supranim
-import ./core/supplier
-export supplier
