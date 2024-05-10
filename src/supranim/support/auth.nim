46f2f05db41003336f3596961e4b75c564306992
George Lemon <georgelemon@protonmail.com>
2024-05-10 21:56:18 +0300

    update `support/auth`
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim/support/auth.nim
+++ src/supranim/support/auth.nim
@@ -1,7 +1,8 @@
+import pkg/valido
 import pkg/libsodium/[sodium, sodium_sizes]
 
-proc hash*(pw: string): string =
+proc hashPassword*(pw: string): string =
   crypto_pwhash_str(pw)
 
-proc check*(pw, hash: string): bool =
+proc checkPassword*(pw, hash: string): bool =
   crypto_pwhash_str_verify(hash, pw)
