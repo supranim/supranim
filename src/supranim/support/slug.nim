faa0cce1e04eee388beb08697bb433fabe3f9bc7
George Lemon <georgelemon@protonmail.com>
2025-02-15 16:23:21 +0200

    fix slug
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim/support/slug.nim
+++ src/supranim/support/slug.nim
@@ -2,26 +2,26 @@ import std/[unidecode, strutils]
 
 proc slugify*(str: string, sep: static char = '-'): string =
   ## Convert `input` string to a ascii slug
-  var str = unidecode(str.strip)
-  result = newStringOfCap(str.len)
+  var x = unidecode(str.strip())
+  result = newStringOfCap(x.len)
   var i = 0
-  while i < str.len:    
-    case str[i]
+  while i < x.len:
+    case x[i]
     of Whitespace:
       inc i
       try:
-        while str[i] notin IdentChars:
+        while x[i] notin IdentChars:
           inc i
         add result, sep
       except IndexDefect: discard
     of PunctuationChars:
       inc i
       try:
-        while str[i] notin IdentChars:
+        while x[i] notin IdentChars:
           inc i
         add result, sep
       except IndexDefect:
         discard
     else:
-      add result, str[i].toLowerAscii
+      add result, x[i].toLowerAscii
       inc i
