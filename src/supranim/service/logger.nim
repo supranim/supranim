10988592017d73cb06f2ec31182ddba7d40beb87
George Lemon <georgelemon@protonmail.com>
2026-03-08 12:04:25 +0200

    todo implement runtime paths
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/supranim/service/logger.nim
+++ src/supranim/service/logger.nim
@@ -6,11 +6,11 @@
 
 import pkg/supranim/core/servicemanager
 
-initService Logger[Global]:
-  ## A thread-based web-service for handling
-  ## logging in the web application.
-  backend do:
-    import std/[logging, os]
-    import pkg/supranim/core/paths
-    var httpErrorsFile = open(logsPath / "http.errors.log", fmWrite)
-    var httpLogger*: FileLogger = newFileLogger(httpErrorsFile)
+# initService Logger[Global]:
+#   ## A thread-based web-service for handling
+#   ## logging in the web application.
+#   backend do:
+#     import std/[logging, os]
+#     import pkg/supranim/core/paths
+#     var httpErrorsFile = open(logsPath / "http.errors.log", fmWrite)
+#     var httpLogger*: FileLogger = newFileLogger(httpErrorsFile)
